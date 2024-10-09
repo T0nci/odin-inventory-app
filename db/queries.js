8b8c97dca8737528921a3cd355f5b8a29e3af89f
T0nci <138928016+T0nci@users.noreply.github.com>
2024-10-09 20:47:34 +0200

    fix: Fix selected queries to return only genres

--- db/queries.js
+++ db/queries.js
@@ -48,7 +48,7 @@ const getAllGames = async () => {
       [row.id],
     );
 
-    games.push({ ...row, ...formatRelations(gameInfo.rows) });
+    games.push({ ...row, genres: gameInfo.map((row) => row.category) });
   }
 
   return games;
@@ -70,11 +70,11 @@ const getGamesByCategoryId = async (categoryId) => {
   const games = [];
   for (const row of rows) {
     const gameInfo = await db.query(
-      "SELECT category, type FROM categories JOIN types ON categories.type_id = types.id JOIN game_relations ON categories.id = game_relations.category_id WHERE game_id = $1",
+      "SELECT category, type FROM categories JOIN types ON categories.type_id = types.id JOIN game_relations ON categories.id = game_relations.category_id WHERE type = 'genre' AND game_id = $1",
       [row.id],
     );
 
-    games.push({ ...row, ...formatRelations(gameInfo.rows) });
+    games.push({ ...row, genres: gameInfo.map((row) => row.category) });
   }
 
   return games;
