424164a89c211f06079cf8210d139e0a9071d49d
T0nci <138928016+T0nci@users.noreply.github.com>
2024-10-09 17:02:15 +0200

    Add error handling

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("node:path");
-const indexRouter = require("./routes/indexRouter.js");
+const indexRouter = require("./routes/indexRouter");
+const links = require("./utils/links");
+const CustomError = require("./utils/CustomError");
 
 const app = express();
 
@@ -11,6 +13,19 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
+
+// If no router matches the request it's a 404.
+// If the request is error marked this will be skipped because it's a non error
+// handler.
+app.use(() => {
+  throw new CustomError("Not Found", 404);
+});
+// Custom error handler for every error
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.render("error", { links, err });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
